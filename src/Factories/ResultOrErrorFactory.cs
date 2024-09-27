1619d65074e5efee48ab97a4daa24532eac25bda
Iraklis <tsikasiraklis@gmail.com>
2024-09-27 13:29:45 +0300

    Add partial error or result expansion

---
+++ src/Factories/ResultOrErrorFactory.cs
@@ -0,0 +1,12 @@
+﻿using ResultOrError.Partials;
+
+namespace ResultOrError.Factories
+{
+    public static class ResultOrErrorFactory
+    {
+        public static ResultOrError<TValue> From<TValue>(TValue value)
+        {
+            return value;
+        }
+    }
+}
---
+++ src/Partials/ResultOrErrorExtension.cs
@@ -0,0 +1,43 @@
+﻿using ResultOrError.Models;
+
+namespace ResultOrError.Partials
+{
+    public static partial class ResultOrErrorExtension
+    {
+        /// <summary>
+        /// Creates an ResultOrError instance with the given value/>.
+        /// </summary>
+        public static ResultOrError<TValue> ToResultOrError<TValue>(this TValue value)
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Creates an ResultOrError instance with the given error/>.
+        /// </summary>
+        public static ResultOrError<TValue> ToResultOrError<TValue>(this Error error)
+        {
+            return error;
+        }
+
+        /// <summary>
+        /// Creates an ResultOrError instance with the given errors/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when errors/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when errors /> is an empty list.</exception>
+        public static ResultOrError<TValue> ToResultOrError<TValue>(this List<Error> errors)
+        {
+            return errors;
+        }
+
+        /// <summary>
+        /// Creates an ResultOrError instance with the given errors/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when errors/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when errors /> is an empty array.</exception>
+        public static ResultOrError<TValue> ToResultOrError<TValue>(this Error[] errors)
+        {
+            return errors;
+        }
+    }
+}
